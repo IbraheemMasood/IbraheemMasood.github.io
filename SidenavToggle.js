00a4c86e6c458e1b951e3707ea2cf3cc9082cff7
IbraheemMasood <Ibraheemmasood10@gmail.com>
2023-02-27 18:21:35 -0800

    Initial commit

---
+++ SidenavToggle.js
@@ -0,0 +1,21 @@
+window.onload = function () {
+    document.getElementById("opener").addEventListener("click", openNav)
+    document.getElementById("closebtn").addEventListener("click", closeNav)
+}
+
+
+/* Set the width of the side navigation to 250 */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.borderRightStyle= "solid";
+    document.getElementById("closebtn").style.fontSize = "36px";
+    console.log("A")
+
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("closebtn").style.fontSize = "0";
+    document.getElementById("mySidenav").style.borderRightStyle= "none";
+}
