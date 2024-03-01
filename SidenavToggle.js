05c07610bef7280cd2df7df4e446de83d5ea5322
IbraheemMasood <Ibraheemmasood10@gmail.com>
2024-02-29 22:26:44 -0800

    Added darkmode toggle Changed style

--- SidenavToggle.js
+++ SidenavToggle.js
@@ -1,13 +1,16 @@
 window.onload = function () {
     document.getElementById("opener").addEventListener("click", openNav)
     document.getElementById("closebtn").addEventListener("click", closeNav)
+    document.getElementById("darkmodeToggle").addEventListener("click", toggleLight)
 }
+let light = true;
 
 
 /* Set the width of the side navigation to 250 */
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("mySidenav").style.borderRightStyle= "solid";
+    document.getElementById("mySidenav").style.color = "#F8F4F9";
+    document.getElementById("mySidenav").style.borderRightStyle = "solid";
     document.getElementById("closebtn").style.fontSize = "36px";
 }
 
@@ -15,5 +18,44 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("closebtn").style.fontSize = "0";
-    document.getElementById("mySidenav").style.borderRightStyle= "none";
+    document.getElementById("mySidenav").style.borderRightStyle = "none";
+}
+
+function toggleLight() {
+
+    if (light) {
+        document.body.style.backgroundColor = "#000000"
+        document.getElementById("resumeContainer").style.border = "2px solid #F8F4F9"
+        document.getElementById("topBar").style.backgroundColor = "#000000"
+        document.getElementById("topBar").style.color = "#F8F4F9"
+        document.getElementById("left").style.backgroundColor = "#000000"
+        document.getElementById("center").style.backgroundColor = "#000000"
+        document.getElementById("right").style.backgroundColor = "#000000"
+
+        for (let i = 0; i < document.getElementsByClassName("title").length; i++) {
+            document.getElementsByClassName("title")[i].style.color = "#F8F4F9";
+        }
+
+
+        document.getElementById("darkmodeToggle").style.color = "#F8F4F9"
+        document.getElementById("darkmodeToggle").textContent = "light mode"
+        light = false
+    } else {
+        document.body.style.backgroundColor = "#F8F4F9"
+        document.getElementById("resumeContainer").style.border = "2px solid #000000"
+        document.getElementById("topBar").style.backgroundColor = "#F8F4F9"
+        document.getElementById("topBar").style.color = "#000000"
+        document.getElementById("left").style.backgroundColor = "#F8F4F9"
+        document.getElementById("center").style.backgroundColor = "#F8F4F9"
+        document.getElementById("right").style.backgroundColor = "#F8F4F9"
+
+        for (let i = 0; i < document.getElementsByClassName("title").length; i++) {
+            document.getElementsByClassName("title")[i].style.color = "#000000";
+        }
+
+
+        document.getElementById("darkmodeToggle").style.color = "#000000"
+        document.getElementById("darkmodeToggle").textContent = "dark mode"
+        light = true;
+    }
 }
